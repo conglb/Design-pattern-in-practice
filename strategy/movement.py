f199c1781ca7f8767abcae26b84321dbda1326ae
Le Ba Cong <conglb@yahoo.com>
2019-04-20 22:20:26 +0700

    Add strategy pattern: movement module

---
+++ strategy/movement.py
@@ -0,0 +1,41 @@
+"""
+This example usuallys is used in game architectured such as Mario, Booberman
+
+"""
+
+class Enemy()
+
+    self.agent = MovementStrategy()
+    def __init__(agent):
+        self.agent = 
+
+    def move():
+
+
+class MovementStrategy(object):
+    
+    def move():
+
+class AI(MovementStrategy):
+    
+    def move():
+
+class DeepLearning(MovementStrategy):
+    
+    def move():
+
+class Naive(MovementStrategy):
+
+    def move():
+
+class EnemyFactory(object):
+    def __init__(self):
+        self.enemy = []
+
+    def create(agent):
+        if agent = 'AI':
+            agent = AI()
+            self.enemy.push_back(Enemy(AI))
+        elif agent = 'Naive':
+            agent = Naive()
+            self.enemy.push_back(Enemy(Naive))
